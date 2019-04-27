81ea3d267315d7c158196dee2cc5a4e97c719098
JONHTAN MOTA DOS REIS <124374910@labin.canoas>
2019-04-26 21:50:11 -0300

    Implementado as urls linkadas

--- app_noticias/urls.py
+++ app_noticias/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, detalhes, noticias_resumo, noticias_resumo_template, noticia_detalhes, slug_view
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('post/<int:pk>/', views.detalhes, name='fullpost')
+    path('', HomePageView.as_view(), name='home'),
+    path('post/<int:pk>/', detalhes, name='fullpost'),
+    path('noticias/<int:noticia_id>', noticia_detalhes, name="detalhes"),
+    path('noticias/resumo/', noticias_resumo, name='resumo'),
+    path('noticias/resumo2/', noticias_resumo_template, name='resumo2'),
+    path('noticias/<str:pk>', slug_view, name='slug')
 ]
 
 
--- app_noticias/views.py
+++ app_noticias/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -10,3 +11,34 @@ class HomePageView(ListView):
 def detalhes(request, pk):
     post = get_object_or_404(Noticia, pk=pk)
     return render(request, 'app_noticias/detalhes.html', {'detalhes': post})
+
+def noticia_detalhes(request, noticia_id):
+    try:
+        noticia = Noticia.objects.get(pk=noticia_id)
+    except Noticia.DoesNotExist:
+        raise Http404('Notícia não encontrada.')
+    return render(request, 'app_noticias/detalhes.html', {'noticia': noticia})
+
+def slug_view(request, pk): #Arrumar
+    try:
+        noticia = Noticia.objects.get(tags=pk)
+    except Noticia.DoesNotExist:
+        raise Http404('Notícias não encontradas.')
+    return render(request, 'app_noticias/slug.html', {'noticia': noticia})
+
+def noticias_resumo(request):
+    total = Noticia.objects.count()
+    html = """
+    <html>
+    <body>
+    <h1>Resumo</h1>
+    <p>A quantidade total de noticias é {}.</p>
+    </body>
+    </html>
+    """.format(total)
+    return HttpResponse(html)
+
+def noticias_resumo_template(request):
+    total = Noticia.objects.count()
+    return render(request, 'app_noticias/resumo.html', {'total':total})
+
